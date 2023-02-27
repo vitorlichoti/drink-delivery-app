76d42d2616a03d4e00aab27a31092673e03f4f1c
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-02-27 19:24:48 -0300

    Requisito 2

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -2,7 +2,47 @@ import React from 'react';
 
 function Login() {
   return (
-    <h1>LOGIN</h1>
+    <div>
+      <form>
+        <label htmlFor="email">
+          {' '}
+          Login
+          <input
+            type="email"
+            name="email"
+            id="email"
+            data-testid="common_login__input-email"
+          />
+        </label>
+        <label htmlFor="password">
+          {' '}
+          Senha
+          <input
+            type="password"
+            name="password"
+            id="password"
+            data-testid="common_login__input-password"
+          />
+        </label>
+        <button
+          type="submit"
+          data-testid="common_login__button-login"
+        >
+          LOGIN
+        </button>
+      </form>
+      <button
+        type="button"
+        data-testid="common_login__button-register"
+      >
+        Ainda não tenho conta
+      </button>
+      <span
+        data-testid="common_login__element-invalid-email"
+      >
+        Email invalido OCULTARRRR!
+      </span>
+    </div>
   );
 }
 
