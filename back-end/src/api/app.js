06c724173017d1b30b2c9e424f87174e48889d08
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-02-28 19:42:54 -0300

    Adiciona rota POST /register para o frontend

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,12 +4,14 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 
-const loginRoute = require('../database/routes/login.route');
+const loginRoute = require('./routes/login.route');
+const registerRoute = require('./routes/register.route');
 
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
 
 app.use(loginRoute);
+app.use(registerRoute);
 
 module.exports = app;
---
+++ back-end/src/api/services/login.service.js
@@ -0,0 +1,26 @@
+const md5 = require('md5');
+const { Users } = require('../../database/models');
+const tokenGenerator = require('../JWT/createJWT');
+
+const getUserEmail = (email) => Users.findOne({ where: { email } });
+
+const checkLogin = async (email, password) => {
+  const userInfo = await getUserEmail(email);
+
+  if (!userInfo) return { type: 404, message: 'User not found' };
+
+  const cryptPassword = md5(password);
+
+  if (cryptPassword === userInfo.password) {
+    const { role } = userInfo;
+    const data = { role };
+    const token = tokenGenerator(data);
+    return { type: 200, message: { token } };
+  }
+
+  return { type: 404, message: 'Incorrect password' };
+};
+
+module.exports = {
+  checkLogin,
+};
