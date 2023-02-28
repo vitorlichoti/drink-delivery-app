ea1da4feb931ba2567938689af0207b5b0eae389
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-02-28 17:35:06 -0300

    Arruma lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -6,7 +6,6 @@ app.use(cors());
 
 const loginRoute = require('../database/routes/login.route');
 
-const app = express();
 app.use(express.json());
 
 app.get('/coffee', (_req, res) => res.status(418).end());
