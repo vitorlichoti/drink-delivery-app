a22ddd0df64f53a38a451499795bb580fdca04bc
Matheus Martinez <decretomusic@gmail.com>
2023-03-02 20:35:07 -0300

    fix: role

--- front-end/src/components/NavBar.jsx
+++ front-end/src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import { Link, useNavigate } from 'react-router-dom';
 import { readStorage, removeToken } from '../utils/localStorage';
 
 function NavBar() {
-  const { name } = readStorage();
+  const { name, role } = readStorage();
   const navigate = useNavigate();
 
   return (
@@ -18,33 +18,25 @@ function NavBar() {
               Produtos
             </Link>)}
       </div>
-      <menu>
-        <Link
-          to="/customer/products"
-          data-testid="customer_products__element-navbar-link-products"
-        >
-          Produtos
-        </Link>
-        <Link
-          to="/customer/orders"
-          data-testid="customer_products__element-navbar-link-orders"
-        >
-          Pedidos
-        </Link>
-        <div data-testid="customer_products__element-navbar-user-full-name">
-          {name}
-        </div>
-        <button
-          data-testid="customer_products__element-navbar-link-logout"
-          type="button"
-          onClick={ () => {
-            removeToken();
-            navigate('/login');
-          } }
-        >
-          Sair
-        </button>
-      </menu>
+      <Link
+        to={ `/${role}/orders` }
+        data-testid="customer_products__element-navbar-link-orders"
+      >
+        Pedidos
+      </Link>
+      <div data-testid="customer_products__element-navbar-user-full-name">
+        {name}
+      </div>
+      <button
+        data-testid="customer_products__element-navbar-link-logout"
+        type="button"
+        onClick={ () => {
+          removeToken();
+          navigate('/login');
+        } }
+      >
+        Sair
+      </button>
     </menu>
   );
 }
