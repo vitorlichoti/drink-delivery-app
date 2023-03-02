72b5a4cbe2a6bf9b91132996acd266866e4e7417
Vitor Lichoti <dev.itor@pop-os.localdomain>
2023-03-02 16:15:59 -0400

    Feat: Adiciona card com data-testid

--- front-end/src/pages/Orders.js
+++ front-end/src/pages/Orders.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import OrdersCards from '../components/OrdersCards';
 
 function Orders() {
   return (
     <div>
       <NavBar />
+      <OrdersCards />
     </div>
   );
 }
