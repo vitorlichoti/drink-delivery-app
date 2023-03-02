ae0cffd0e5885670fd72b9e97b86f30ac0c17386
Vinicius Luiz de Souza Reis <viniciusluizdesouzareis@pop-os.localdomain>
2023-03-02 16:00:26 -0300

    a

--- front-end/src/utils/localStorage.js
+++ front-end/src/utils/localStorage.js
@@ -1,6 +1,7 @@
 // import axios from "axios";
 
 const USER_STORAGE_KEY = 'user';
+const CART_STORAGE_KEY = 'userCart';
 
 function readStorage() {
   return JSON.parse(localStorage.getItem(USER_STORAGE_KEY) || '[]');
@@ -14,22 +15,27 @@ function getCartProducts() {
   return readStorage();
 }
 
-function addProductToCart(product) {
-  const productss = readStorage();
-  const alreadyInProduct = productss.find(({ id }) => id === product.id);
-  if (alreadyInProduct) {
-    writeStorage(
-      productss.map((currentProduct) => ({
-        ...currentProduct,
-        length:
-          currentProduct.id === product.id
-            ? currentProduct.length + 1
-            : currentProduct.length,
-      })),
-    );
-    return;
-  }
-  writeStorage([...productss, { ...product, length: 1 }]);
+function addProductToCart(product, qntt) {
+  console.log('a');
+  // const products = JSON.parse(localStorage.getItem(CART_STORAGE_KEY) || '[]');
+  // if (products.length === 0) {
+  //   localStorage.setItem(CART_STORAGE_KEY, JSON.stringify(product));
+  // }
+  // console.log(products);
+  // const alreadyInProduct = products.find(({ id }) => id === product.id);
+  // if (alreadyInProduct) {
+  //   writeStorage(
+  //     products.map((currentProduct) => ({
+  //       ...currentProduct,
+  //       length:
+  //         currentProduct.id === product.id
+  //           ? currentProduct.length + 1 * `${qntt}`
+  //           : currentProduct.length,
+  //     })),
+  //   );
+  //   return;
+  // }
+  // writeStorage([...products, { ...product, length: 1 }]);
 }
 
 function removeToken() {
@@ -51,12 +57,12 @@ function increaseCartProduct(id) {
   );
 }
 
-function decreaseCartProduct(id) {
+function decreaseCartProduct(id, qntt) {
   const products = readStorage();
   writeStorage(
     products.map((product) => ({
       ...product,
-      length: product.id === id ? product.length - 1 : product.length,
+      length: product.id === id ? product.length - qntt : product.length,
     })),
   );
 }
