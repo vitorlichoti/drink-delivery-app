2c9e5c7dce321211e7027ba35d2358b70d6d865e
Vitor Lichoti <dev.itor@pop-os.localdomain>
2023-03-06 17:49:04 -0400

    Feat: cria tabela sem dados dinamicos

--- front-end/src/components/SellerOrderInfo.jsx
+++ front-end/src/components/SellerOrderInfo.jsx
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import httpRequestAxios from '../utils/httpRequestAxios';
+import { readCartStorage } from '../utils/localStorage';
 
 const prefix = 'seller_order_details__';
 
 function SellerOrderInfo() {
   const [sales, setSales] = useState([]);
   const { id } = useParams();
+  const products = readCartStorage();
 
   async function getAllSales() {
     const allSales = await httpRequestAxios('get', 'http://localhost:3001/seller/orders');
@@ -21,9 +23,9 @@ function SellerOrderInfo() {
   useEffect(() => {
     if (!sales.length) onLoad();
   });
-  console.log(sales);
+
   return (
-    <section style={ { display: 'flex', justifyContent: 'space-around' } }>
+    <section>
       <header
         style={ { display: 'flex', justifyContent: 'space-around' } }
       >
@@ -53,6 +55,50 @@ function SellerOrderInfo() {
           SAIU PARA ENTREGA
         </button>
       </header>
+
+      <table style={ { width: '100%' } }>
+        <thead>
+          <tr style={ { display: 'flex', justifyContent: 'space-around' } }>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products?.map((product, index) => (
+            <tr key={ product.id }>
+              <td
+                data-testid={ `${prefix}element-order-table-item-number${index}` }
+              >
+                {index + 1}
+              </td>
+              <td
+                data-testid={ `${prefix}element-order-table-name${index}` }
+              >
+                {product.name}
+              </td>
+              <td
+                data-testid={ `${prefix}element-order-table-quantity${index}` }
+              >
+                {product.quantity}
+              </td>
+              <td
+                data-testid={ `${prefix}element-order-table-unit-price${index}` }
+              >
+                {product.price.replace(/\./, ',')}
+              </td>
+              <td
+                data-testid={ `${prefix}element-order-table-sub-total${index}` }
+              >
+                {(Math.round(Number(product.price * product.quantity) * 100) / 100).toFixed(2).replace(/\./, ',')}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+
       <footer>
         <p data-testid={ `${prefix}element-order-total-price` }>
           { `Total: ${sales[id - 1]?.totalPrice}` }
